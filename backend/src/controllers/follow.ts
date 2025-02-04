43396fa1d6dca623d377229fef603a58b38150a2
“saiprashanth” <saiprashanth751@gmail.com>
2025-02-04 15:31:56 +0530

    added signin

--- backend/src/controllers/follow.ts
+++ backend/src/controllers/follow.ts
@@ -1,6 +1,7 @@
 import { asyncHandler } from "../middleware/asyncHandler";
 import { Request, Response } from "express";
 import { PrismaClient } from "@prisma/client";
+import CustomError from "../utils/customError";
 
 const prisma = new PrismaClient()
 
@@ -12,12 +13,16 @@ export const followUser = asyncHandler(async (req:AuthRequest, res: Response) =>
     const userId = req.id as string
     const tofollow = req.params.id;
 
-    await prisma.follow.create({
+    const follow = await prisma.follow.create({
         data:{
             followerId: userId,
             followingId: tofollow
         }
     })
+
+    if(!follow){
+        throw new CustomError("User already follwed", 400)
+    }
 
     return res.status(200).json({
         message: "Follower Updated"
@@ -36,4 +41,44 @@ export const unfollowUser = asyncHandler(async (req: AuthRequest, res: Response)
             }
         }
     })
+    
+    return res.status(200).json({
+        message: "User successfully unfollwed"
+    })
 })
+
+export const getFollowers = asyncHandler(async (req: AuthRequest, res:Response) => {
+    const userId = req.id as string
+    const followers = await prisma.follow.findMany({
+        where:{
+            followingId: userId,
+        }
+    })
+
+    if(!followers){
+        throw new CustomError("No followers", 400)
+    }
+
+    return res.status(200).json({
+        followers
+    })
+})
+
+export const getFollowing = asyncHandler(async(req:AuthRequest, res:Response) => {
+    const userId = req.id as string
+    const followingUsers = await prisma.follow.findMany({
+        where: {
+            followerId: userId,
+        }
+    })
+    if(!followingUsers){
+        throw new CustomError("User is not following anyone", 400)
+    }
+    
+    return res.status(200).json({
+        followingUsers
+    })
+
+})
+
+
