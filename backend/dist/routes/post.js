bd6ba773a611e153dceee897eb91578a6e3bb46d
“saiprashanth” <saiprashanth751@gmail.com>
2025-02-15 04:34:42 +0530

    fixed auth-validation

--- backend/dist/routes/post.js
+++ backend/dist/routes/post.js
@@ -5,10 +5,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const post_1 = require("../controllers/post");
+const authMiddleware_1 = require("../middleware/authMiddleware");
 const app = express_1.default.Router();
-app.post("/userPost", post_1.createPost);
+app.post("/userPost", authMiddleware_1.authMiddleware, post_1.createPost);
 app.put("/updatePost", post_1.updatePost);
-app.get("/userAll", post_1.getUserPosts);
+app.get("/userAll", authMiddleware_1.authMiddleware, post_1.getUserPosts);
 app.get("/all", post_1.getAllPosts);
 app.get("/:id", post_1.getPostById);
 exports.default = app;
--- backend/src/routes/post.ts
+++ backend/src/routes/post.ts
@@ -1,11 +1,12 @@
 import  express  from "express";
 import { createPost, getAllPosts, getPostById, getUserPosts, updatePost } from "../controllers/post";
+import { authMiddleware } from "../middleware/authMiddleware";
 
 const app = express.Router();
 
-app.post("/userPost",createPost);
+app.post("/userPost",authMiddleware, createPost);
 app.put("/updatePost",updatePost);
-app.get("/userAll",getUserPosts);
+app.get("/userAll",authMiddleware, getUserPosts);
 app.get("/all",getAllPosts);
 app.get("/:id",getPostById);
 
