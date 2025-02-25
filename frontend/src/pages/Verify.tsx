01dcf7ec879cb617b6986a976eba5987b1a4c422
“saiprashanth” <saiprashanth751@gmail.com>
2025-02-25 22:50:23 +0530

    fixed: redirect-route

--- frontend/src/pages/Verify.tsx
+++ frontend/src/pages/Verify.tsx
@@ -4,7 +4,7 @@ export default function Verify() {
         <div className="flex flex-col justify-center h-screen items-center bg-slate-300">
             <div className="mb-16 text-3xl font-bold">Connect!</div>
             <div className="text-xl max-w-xl font-semibold text-center shadow-white shadow-2xl rounded-xl p-9 bg-gradient-to-r from-white to-slate-200 ">
-                Please verify your <span className="underline">Email</span> via the verification link that has sent to your registered University Mail ID
+                Please verify your <span className="underline">Email</span> via the verification link that has sent to your registered Email ID
             </div>
         </div>
     )
